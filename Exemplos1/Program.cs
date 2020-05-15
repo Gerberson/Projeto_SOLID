ea295ea7967be9a1a490cdde48ef0aaa9b382126
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-14 23:51:11 -0300

    Métodos : Parâmetros Opcionais com PARAMS

--- Exemplos1/Program.cs
+++ Exemplos1/Program.cs
@@ -10,16 +10,29 @@ namespace Exemplos1
             var resultado2 = Somar(100, 200);
             var resultado3 = Somar(100, 200, 300);
             var resultado4 = Somar(100, 200, 300, 400); 
+            var resultado5 = Somar(100, 200, 300, 400, 500); 
+            var resultado6 = Somar(100, 200, 300, 400, 500, 600); 
+            var resultado7 = Somar(100, 200, 300, 400, 500, 600, 700); 
             Console.WriteLine(resultado1);
             Console.WriteLine(resultado2);
             Console.WriteLine(resultado3);
             Console.WriteLine(resultado4);
+            Console.WriteLine(resultado5);
+            Console.WriteLine(resultado6);
+            Console.WriteLine(resultado7);
         }
 
         //PARAMENTRO OPCIONAIS DEVE VIR DEPOIS DO PRIMEIRO PARAMENTRO OBRIGATORIO
-        static public int Somar(int x, int y = 10, int z = 20, int w = 30)
+        //UTILIZANDO O PARAMS NÃO PRECISAMOS DEFINIR VALOR PARADRÃO
+        static public int Somar(int x, params int[] numeros)
         {
-            return x + y + z + w;
+            int result = 0;
+            foreach (int num in numeros)
+            {
+                result += num;
+            }
+
+            return x + result;
         }
     }
 }
