e7425d13f56d172278d7c983f4f385687f414ac8
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-15 00:04:05 -0300

    Métodos : Argumentos Nomeados

--- Exemplos1/Program.cs
+++ Exemplos1/Program.cs
@@ -6,33 +6,15 @@ namespace Exemplos1
     {
         static void Main(string[] args)
         {
-            var resultado1 = Somar(100);
-            var resultado2 = Somar(100, 200);
-            var resultado3 = Somar(100, 200, 300);
-            var resultado4 = Somar(100, 200, 300, 400); 
-            var resultado5 = Somar(100, 200, 300, 400, 500); 
-            var resultado6 = Somar(100, 200, 300, 400, 500, 600); 
-            var resultado7 = Somar(100, 200, 300, 400, 500, 600, 700); 
-            Console.WriteLine(resultado1);
-            Console.WriteLine(resultado2);
-            Console.WriteLine(resultado3);
-            Console.WriteLine(resultado4);
-            Console.WriteLine(resultado5);
-            Console.WriteLine(resultado6);
-            Console.WriteLine(resultado7);
+            EnviarEmail(destino: "destino@gmail.com", assunto: "Teste", titulo: "Urgente");
+            EnviarEmail(titulo: "Urgente", assunto: "Teste", destino: "destino@gmail.com");
+            EnviarEmail(assunto: "Teste", destino: "destino@gmail.com", titulo: "Urgente");
         }
 
-        //PARAMENTRO OPCIONAIS DEVE VIR DEPOIS DO PRIMEIRO PARAMENTRO OBRIGATORIO
-        //UTILIZANDO O PARAMS NÃO PRECISAMOS DEFINIR VALOR PARADRÃO
-        static public int Somar(int x, params int[] numeros)
+        //PARAMENTRO NOMEADOS VOCÊ NÃO PRECISA RESPEITAR A ORDEM EM QUE OS PARAMETROS FOI DEFINIDO
+        static void EnviarEmail(string destino, string titulo, string assunto)
         {
-            int result = 0;
-            foreach (int num in numeros)
-            {
-                result += num;
-            }
-
-            return x + result;
+            Console.WriteLine($"{destino} - {titulo} - {assunto}");
         }
     }
 }
