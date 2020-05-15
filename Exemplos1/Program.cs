0b86f7b65745144448ae31dda3a1dbcddcdf70a0
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-14 23:44:34 -0300

    Métodos : Parâmetros Opcionais

--- Exemplos1/Program.cs
+++ Exemplos1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Exemplos1
 {
@@ -7,43 +6,20 @@ namespace Exemplos1
     {
         static void Main(string[] args)
         {
-            var resultado1 = Somar(100, 200);
-            var resultado2 = Somar(100, 200, 300);
-            var resultado3 = Somar(1, 2, 3, 4);
-            var resultado4 = Somar(new int[] { 1, 2, 3, 4 }); 
+            var resultado1 = Somar(100);
+            var resultado2 = Somar(100, 200);
+            var resultado3 = Somar(100, 200, 300);
+            var resultado4 = Somar(100, 200, 300, 400); 
             Console.WriteLine(resultado1);
             Console.WriteLine(resultado2);
             Console.WriteLine(resultado3);
             Console.WriteLine(resultado4);
-
-
-
         }
 
-        static public int Somar(int numero1, int numero2)
+        //PARAMENTRO OPCIONAIS DEVE VIR DEPOIS DO PRIMEIRO PARAMENTRO OBRIGATORIO
+        static public int Somar(int x, int y = 10, int z = 20, int w = 30)
         {
-            return numero1 + numero2;
-        }
-
-        //SOBRE CARGA DE METODOS SO É POSSIVEL SE O PARAMETRO MUDAR
-        static public double Somar(double numero1, int numero2)
-        {
-            return numero1 + numero2;
-        }
-
-        static public int Somar(int numero1, int numero2, int numero3)
-        {
-            return numero1 + numero2 + numero3;
-        }
-
-        static public int Somar(params int[] numeros)
-        {
-            var resultado = 0;
-            foreach (int num in numeros)
-            {
-                resultado += num;
-            }
-            return resultado;
+            return x + y + z + w;
         }
     }
 }
