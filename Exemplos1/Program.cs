f4a55b1d9c9d25f3a806a5b39fa86d11a4da47f6
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-14 23:14:40 -0300

    Palavra THIS

---
+++ Exemplos1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exemplos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Exemplo("Teste da palavra THIS"));
+        }
+    }
+
+    class Teste
+    {
+        public string Nome { get; set; }
+
+        public Teste(Exemplo exemplo)
+        {
+            Nome = exemplo.Fornecedor;
+        }
+    }
+
+    class Exemplo
+    {
+        public string Fornecedor { get; set; }
+
+        public Exemplo(string nome)
+        {
+            Fornecedor = nome;
+
+            Teste teste = new Teste(this);
+
+            Console.WriteLine(teste.Nome);
+        }
+    }
+}
