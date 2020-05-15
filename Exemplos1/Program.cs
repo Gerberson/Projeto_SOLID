7f02e95e5bb6446b861e9bd85c44d34f44c11d0a
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-15 16:19:56 -0300

    Interface: Acoplado

--- Exemplos1/Program.cs
+++ Exemplos1/Program.cs
@@ -6,21 +6,11 @@ namespace Exemplos1
     {
         static void Main(string[] args)
         {
-            Quadrado q = new Quadrado();
-            Console.WriteLine(q.Descricao());
+            RegistrarOcorrencias registrarOcorrencias = new RegistrarOcorrencias();
 
-            Console.WriteLine("Informe a cor do Quadrado:");
-            q.Cor = Console.ReadLine();
-
-            Console.WriteLine("Informe o lado do Quadrado:");
-            q.Lado = double.Parse(Console.ReadLine());
-
-            q.CalcularArea();
-            q.CalcularPerimetro();
-
-            Console.WriteLine(q.Area);
-            Console.WriteLine(q.Perimetro);
-            Console.WriteLine(q.Cor);
+            registrarOcorrencias.RegistrarNoConsole("Registrei no console");
+            registrarOcorrencias.RegistrarNoArquivo("Arquivo", "Registrei no arquivo.");
+            registrarOcorrencias.RegistrarJSON("JSON", "Registrei no JSON.");
         }
     }
 }
---
+++ Exemplos1/RegistrarOcorrencias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplos1
+{
+    class RegistrarOcorrencias
+    {
+        public void RegistrarNoConsole(string mensagem)
+        {
+            Console.WriteLine($"{mensagem} : {DateTime.Now}");
+        }
+
+        public void RegistrarNoArquivo(string arquivo, string mensagem)
+        {
+            Console.WriteLine($"{arquivo} - {mensagem}");
+        }
+
+        public void RegistrarJSON(string arquivo, string mensagem)
+        {
+            Console.WriteLine($"{arquivo} - {mensagem}");
+        }
+    }
+}
