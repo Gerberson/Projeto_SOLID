334239718547bd7ee919f33147c4caa3c8cc8b57
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-17 18:55:26 -0300

    Exercicio 1 Violacao: Aplicando Enum Para Tipos de Clientes - SOLID

---
+++ Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/EStatusContaCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplos1._2_EXERCICIOS_SOLID.Exercicio_1.Aderente.Entitie
+{
+    enum EStatusContaCliente
+    {
+        NaoRegistrado = 1,
+        ClienteComum = 2,
+        ClienteEsprecial = 3,
+        ClienteVIP = 4
+    }
+}
--- Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/GerenciadorDeDesconto.cs
+++ Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/GerenciadorDeDesconto.cs
@@ -6,28 +6,30 @@ namespace Exemplos1._2_EXERCICIOS_SOLID.Exercicio_1.Aderente.Entitie
 {
     class GerenciadorDedescontoPorFidelidadeonto
     {
-        public decimal AplicardescontoPorFidelidadeonto(decimal precoProduto, int statusContaCliente, int tempoDeContaEmAnos)
+        public decimal AplicardescontoPorFidelidadeonto(decimal precoProduto, EStatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
         {
             decimal precoAposdesconto = 0;
 
             decimal descontoPorFidelidade = (tempoDeContaEmAnos > 5) ? (decimal)5 / 100 : (decimal)tempoDeContaEmAnos / 100;
 
-            if (statusContaCliente == 1)
+            switch (statusContaCliente)
             {
-                precoAposdesconto = precoProduto;
+                case EStatusContaCliente.NaoRegistrado:
+                    precoAposdesconto = precoProduto;
+                    break;
+                case EStatusContaCliente.ClienteComum:
+                    precoAposdesconto = (precoProduto - (0.1m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
+                    break;
+                case EStatusContaCliente.ClienteEsprecial:
+                    precoAposdesconto = (0.7m * precoProduto) - descontoPorFidelidade * (0.7m * precoProduto);
+                    break;
+                case EStatusContaCliente.ClienteVIP:
+                    precoAposdesconto = (precoProduto - (0.5m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+                    break;
+                default:
+                    throw new NotImplementedException();
             }
-            else if (statusContaCliente == 2)
-            {
-                precoAposdesconto = (precoProduto - (0.1m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
-            }
-            else if (statusContaCliente == 3)
-            {
-                precoAposdesconto = (0.7m * precoProduto) - descontoPorFidelidade * (0.7m * precoProduto);
-            }
-            else if (statusContaCliente == 4)
-            {
-                precoAposdesconto = (precoProduto - (0.5m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
-            }
+
             return precoAposdesconto;
         }
     }
