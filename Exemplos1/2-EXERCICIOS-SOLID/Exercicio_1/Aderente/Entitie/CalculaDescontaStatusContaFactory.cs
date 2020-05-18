157863971f54c558b8d625b398bc47b769ca8e59
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-05-17 22:25:57 -0300

    Exercicio 1 Aderente: Resultado Final - SOLID

---
+++ Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/CalculaDescontaStatusContaFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplos1._2_EXERCICIOS_SOLID.Exercicio_1.Aderente.Entitie
+{
+    class CalculaDescontaStatusContaFactory : ICalculaDescontoStatusContaFactory
+    {
+        public ICalculaDescontoStatusConta GetCalculaDescontoStatusConta(EStatusContaCliente statusContaClientes)
+        {
+            ICalculaDescontoStatusConta calcular;
+            switch (statusContaClientes)
+            {
+                case EStatusContaCliente.NaoRegistrado:
+                    calcular = new ClienteNaoRegistrado();
+                    break;
+                case EStatusContaCliente.ClienteComum:
+                    calcular = new ClienteComum();
+                    break;
+                case EStatusContaCliente.ClienteEsprecial:
+                    calcular = new ClienteEspecial();
+                    break;
+                case EStatusContaCliente.ClienteVIP:
+                    calcular = new ClienteVIP();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return calcular;
+        }
+    }
+}
--- Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/GerenciadorDeDesconto.cs
+++ Exemplos1/2-EXERCICIOS-SOLID/Exercicio_1/Aderente/Entitie/GerenciadorDeDesconto.cs
@@ -1,40 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Exemplos1._2_EXERCICIOS_SOLID.Exercicio_1.Aderente.Entitie
 {
     class GerenciadorDedesconto
     {
         private readonly ICalculaDescontoFidelidade _calculaDescontoFidelidade;
-        public GerenciadorDedesconto(ICalculaDescontoFidelidade calculaDescontoFidelidade)
+        private readonly ICalculaDescontoStatusContaFactory _descontoStatusConta;
+        public GerenciadorDedesconto(ICalculaDescontoFidelidade calculaDescontoFidelidade, ICalculaDescontoStatusContaFactory descontoStatusConta)
         {
+            _descontoStatusConta = descontoStatusConta;
             _calculaDescontoFidelidade = calculaDescontoFidelidade;
         }
         public decimal AplicardescontoPorFidelidadeonto(decimal precoProduto, EStatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
         {
             decimal precoAposdesconto = 0;
 
-            switch (statusContaCliente)
-            {
-                case EStatusContaCliente.NaoRegistrado:
-                    precoAposdesconto = new ClienteNaoRegistrado().AplicarDescontoStatusConta(precoProduto);
-                    break;
-                case EStatusContaCliente.ClienteComum:
-                    precoAposdesconto = new ClienteComum().AplicarDescontoStatusConta(precoProduto);
-                    precoAposdesconto = _calculaDescontoFidelidade.AplicarDescontoFidelidade(precoAposdesconto, tempoDeContaEmAnos);
-                    break;
-                case EStatusContaCliente.ClienteEsprecial:
-                    precoAposdesconto = new ClienteEspecial().AplicarDescontoStatusConta(precoProduto);
-                    precoAposdesconto = _calculaDescontoFidelidade.AplicarDescontoFidelidade(precoAposdesconto, tempoDeContaEmAnos);
-                    break;
-                case EStatusContaCliente.ClienteVIP:
-                    precoAposdesconto = new ClienteVIP().AplicarDescontoStatusConta(precoProduto);
-                    precoAposdesconto = _calculaDescontoFidelidade.AplicarDescontoFidelidade(precoAposdesconto, tempoDeContaEmAnos);
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
+            precoAposdesconto = _descontoStatusConta
+                .GetCalculaDescontoStatusConta(statusContaCliente)
+                .AplicarDescontoStatusConta(precoProduto);
+            precoAposdesconto = _calculaDescontoFidelidade
+                .AplicarDescontoFidelidade(precoAposdesconto, tempoDeContaEmAnos);
 
             return precoAposdesconto;
         }
